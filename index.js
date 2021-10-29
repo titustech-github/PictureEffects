060cb2d8c2d4610d1bb361c679b4202dfb7c1886
Titus <titus.a@ecgroup-intl.com>
2021-10-29 12:04:25 +0530

    syntax issue fixes

--- index.js
+++ index.js
@@ -2,25 +2,23 @@ function pictureEffects(options) {
     let images = document.querySelectorAll(".image-effects");
 
     images.forEach(image => {
-        //image.style.boxShadow = options.shadow;
 
-        if(options.shadow) {
+        if(options.shadow) 
             image.style.boxShadow = "10px 10px 5px rgba(0,0,0,0.1)"
-        }else 
+        else 
             image.style.boxShadow = "none"
 
-        if(options.padding) {
+        if(options.padding) 
             image.style.padding = "10px"
-        }else 
+        else 
             image.style.padding = "0px"
         
-    
-        if(options.border) {
+        if(options.border) 
             image.style.border = "1px solid red"
-        }else 
+        else 
             image.style.border = "0 none"
         
-    }
+    })
 }
 
 module.export.pictureEffects = pictureEffects;
